9b71a99135a5e4cc5f39ef803b572967b230ba51
echolinr <echo.zhailin@gmail.com>
2017-04-16 11:33:03 -0700

    components

---
+++ project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+import {Recipe} from '../recipes.module';
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test', 'http://food.fnr.sndimg.com/content/dam/images/food/fullset/2011/7/26/0/IG0504_brownie-tart_s4x3.jpg.rend.hgtvcom.966.725.jpeg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ project/src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {Ingredient} from '../shared/ingredient.module';
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [
+    new Ingredient('apples', 5),
+    new Ingredient('tomatoes', 10),
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
